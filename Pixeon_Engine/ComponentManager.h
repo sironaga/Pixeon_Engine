32dea92e740798f4724609827689a88bd8aa1ca4
しろなが <45118937+sironaga@users.noreply.github.com>
2025-09-17 14:02:17 +0900

    Create : Component Manager Class

---
+++ Pixeon_Engine/ComponentManager.h
@@ -0,0 +1,17 @@
+#pragma once
+// コンポーネントマネージャークラス
+// コンポーネントの管理を行う
+
+class ComponentManager
+{
+public:
+
+
+
+private:
+	static ComponentManager* _instance;
+private:
+	ComponentManager() {}
+	~ComponentManager() {}
+};
+
